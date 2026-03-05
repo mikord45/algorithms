984cb33ad7e1f75bd8c4da0b57fdf5700e2de25e
mikord45 <51155742+mikord45@users.noreply.github.com>
2026-03-05 23:04:55 +0100

    chore: linting & formatting improvement

--- src/recursion/index.test.ts
+++ src/recursion/index.test.ts
@@ -1,22 +1,22 @@
 import { getSumOfNaturalNumbersBelow } from "."
 
-describe("Returns correct value", ()=>{
-	it("n <= 1 work correctly", ()=>{
-		expect(getSumOfNaturalNumbersBelow(1)).toBe(0)
-		expect(getSumOfNaturalNumbersBelow(0)).toBe(0)
-		expect(getSumOfNaturalNumbersBelow(-2)).toBe(0)
-	})
+describe("Returns correct value", () => {
+  it("n <= 1 work correctly", () => {
+    expect(getSumOfNaturalNumbersBelow(1)).toBe(0)
+    expect(getSumOfNaturalNumbersBelow(0)).toBe(0)
+    expect(getSumOfNaturalNumbersBelow(-2)).toBe(0)
+  })
 
-	it("fractions work correctly", ()=>{
-		expect(getSumOfNaturalNumbersBelow(1.5)).toBe(1)
-		expect(getSumOfNaturalNumbersBelow(5.5)).toBe(15)
-		expect(getSumOfNaturalNumbersBelow(5.05)).toBe(15)
-		expect(getSumOfNaturalNumbersBelow(5.9)).toBe(15)
-		expect(getSumOfNaturalNumbersBelow(5.95)).toBe(15)
-	})
+  it("fractions work correctly", () => {
+    expect(getSumOfNaturalNumbersBelow(1.5)).toBe(1)
+    expect(getSumOfNaturalNumbersBelow(5.5)).toBe(15)
+    expect(getSumOfNaturalNumbersBelow(5.05)).toBe(15)
+    expect(getSumOfNaturalNumbersBelow(5.9)).toBe(15)
+    expect(getSumOfNaturalNumbersBelow(5.95)).toBe(15)
+  })
 
-	it("standard cases work correctly", ()=>{
-		expect(getSumOfNaturalNumbersBelow(6)).toBe(15)
-		expect(getSumOfNaturalNumbersBelow(9)).toBe(36)
-	})
+  it("standard cases work correctly", () => {
+    expect(getSumOfNaturalNumbersBelow(6)).toBe(15)
+    expect(getSumOfNaturalNumbersBelow(9)).toBe(36)
+  })
 })
--- src/selection-sort/index.ts
+++ src/selection-sort/index.ts
@@ -1,29 +1,29 @@
 type SelectionSearchFn = (list: number[], desc?: boolean) => number[]
 
 const getNextElement = (list: number[], desc?: boolean): { result: number, list: number[] } => {
-	let result = list[0]
-	let resultPosition = 0
+  let result = list[0]
+  let resultPosition = 0
 
-	list.forEach((item, index) => {
-		if ((!desc && item < result) || (desc && item > result)) {
-			result = item
-			resultPosition = index
-		}
-	})
+  list.forEach((item, index) => {
+    if ((!desc && item < result) || (desc && item > result)) {
+      result = item
+      resultPosition = index
+    }
+  })
 
-	list.splice(resultPosition, 1)
-	return { result, list }
+  list.splice(resultPosition, 1)
+  return { result, list }
 }
 
 export const selectionSort: SelectionSearchFn = (list, desc) => {
-	let unsortedList = [...list]
-	const sortedList = []
+  let unsortedList = [...list]
+  const sortedList = []
 
-	while (unsortedList.length > 0) {
-		const { result, list } = getNextElement(unsortedList, desc)
-		sortedList.push(result)
-		unsortedList = [...list]
-	}
+  while (unsortedList.length > 0) {
+    const { result, list } = getNextElement(unsortedList, desc)
+    sortedList.push(result)
+    unsortedList = [...list]
+  }
 
-	return sortedList
+  return sortedList
 }
