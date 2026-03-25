e4c4d43dac8853688fc4b83bc1bc7da12a70db46
mikord45 <51155742+mikord45@users.noreply.github.com>
2026-03-25 22:02:12 +0100

    test: added tests - part 1

--- TODO.md
+++ TODO.md
@@ -1,2 +1,3 @@
-1. Create github actions and include jest tests in them
-2. Research: Tail Recursion
+1. Fix differences between TS checks in VSC and in Jest in custom-map (reason for all those type assertions)
+2. Create github actions and include jest tests in them
+3. Research: Tail Recursion
--- src/custom-map/index.test.ts
+++ src/custom-map/index.test.ts
@@ -1,14 +1,43 @@
 import { CustomMap } from "."
 
-const map = new CustomMap(4)
-map.add("very different key", "value1")
-console.log(map)
-map.add("randomKey", "value3")
-console.log(map)
-map.add("Lorem", "value3")
-console.log(map) 
+describe("Custom Map", () => {
+  it("Scenario 1", ()=>{
+    const map1 = new CustomMap()
 
+    expect(map1.numberOfElementsInTable).toBe(0)
+    expect(map1.table.length).toBe(100)
 
-it("test", ()=>{
-  expect(1).toBe(1)
+    Array.from({length: 10}, (_,index) => {
+      map1.add(`key${index}`, `value${index}`)
+    })
+
+    expect(map1.numberOfElementsInTable).toBe(10)
+    expect(map1.table.length).toBe(100)
+
+    Array.from({length: 10}, (_,index) => {
+      expect(map1.get(`key${index}`)).toEqual({key: `key${index}`, value: `value${index}`})
+    })
+
+    Array.from([2,4,7]).forEach((currentElement)=>{
+      map1.delete(`key${currentElement}`)
+    })
+
+    expect(map1.numberOfElementsInTable).toBe(7)
+
+    map1.add("specialKey1", "specialValue1")
+    map1.add("specialKey2", "specialValue2")
+
+    expect(map1.numberOfElementsInTable).toBe(9)
+    expect(map1.get("specialKey1")).toEqual({key: "specialKey1", value: "specialValue1"})
+    expect(map1.get("specialKey2")).toEqual({key: "specialKey2", value: "specialValue2"})
+
+    Array.from([0,1,3,5,6,8,9], (currentElement) => {
+      expect(map1.get(`key${currentElement}`)).toEqual({key: `key${currentElement}`, value: `value${currentElement}`})
+    })
+
+    Array.from([2,4,7]).forEach((currentElement)=>{
+      expect(map1.get(`key${currentElement}`)).toBe(undefined)
+    })
+  })
 })
+
