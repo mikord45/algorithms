efe8ca4a93fb2174bcfa19c92e8d3bd09e6f7219
mikord45 <51155742+mikord45@users.noreply.github.com>
2026-03-29 23:00:21 +0200

    feat: added topologySortBFS()

--- src/graphs/bfs/index.test.ts
+++ src/graphs/bfs/index.test.ts
@@ -1,4 +1,4 @@
-import { getPathBFS, GraphNode } from "."
+import { getPathBFS, GraphNode, topologySortBFS } from "."
 
 const getDestinationGraphNodes = (destinationExists: boolean) => {
   const node32 = new GraphNode("Piotr", [], destinationExists)
@@ -11,7 +11,7 @@ const getDestinationGraphNodes = (destinationExists: boolean) => {
   const node1 = new GraphNode("Bartek", [node11, node31])
   const startingPoint = new GraphNode("Me", [node1, node2, node3])
 
-  return {startingPoint, node1, node32}
+  return {startingPoint, node1, node32, node31}
 }
 
 
@@ -81,5 +81,116 @@ describe("Infinite Loop", ()=>{
     expect(getPathBFS(node1)).toEqual({
       exists: false
     })
+  })
+})
+
+describe("Topology Sort", () => {
+  it("Throws an error when graph has cycles", () => {
+    const node1 = new GraphNode("Node1", [])
+    const node21 = new GraphNode("Node21", [])
+    const node22 = new GraphNode("Node22", [])
+    const node2 = new GraphNode("Node2", [node21, node22])
+    node1.linkedWith = [node2]
+    node2.linkedWith.push(node1)
+
+    expect(() => topologySortBFS(node1)).toThrowError("Graph has cycles")
+  })
+
+  it("Throws an error when graph has cycles in the middle", () => {
+    const node21 = new GraphNode("Node21", [])
+    const node22 = new GraphNode("Node22", [node21])
+    node21.linkedWith.push(node22)
+    const node2 = new GraphNode("Node2", [node21, node22])
+    const node1 = new GraphNode("Node1", [node2])
+
+    expect(() => topologySortBFS(node1)).toThrowError("Graph has cycles")
+  })
+
+  it("Throws on complex graph with cycles", ()=>{
+    const node41 = new GraphNode("Node41", [])
+    const node31 = new GraphNode("Node31", [node41])
+    const node32 = new GraphNode("Node32", [])
+    const node21 = new GraphNode("Node21", [node31])
+    const node22 = new GraphNode("Node22", [node32])
+    const node1 = new GraphNode("Node1", [node21, node22])
+    node41.linkedWith.push(node32)
+    node32.linkedWith.push(node21)
+
+    expect(() => topologySortBFS(node1)).toThrowError("Graph has cycles")
+  })
+
+  it("Sorts complex graph", ()=>{
+    const node41 = new GraphNode("Node41", [])
+    const node31 = new GraphNode("Node31", [node41])
+    const node32 = new GraphNode("Node32", [])
+    const node21 = new GraphNode("Node21", [node31])
+    const node22 = new GraphNode("Node22", [node32])
+    const node1 = new GraphNode("Node1", [node21, node22])
+    node32.linkedWith.push(node21)
+
+    expect(topologySortBFS(node1).map((current) => current.name)).toEqual(["Node1", "Node22", "Node32", "Node21", "Node31", "Node41"])
+  })
+
+  it("Sorts complex graph 2", ()=>{
+    const node42 = new GraphNode("Node42", [])
+    const node41 = new GraphNode("Node41", [])
+    const node31 = new GraphNode("Node31", [node41])
+    const node32 = new GraphNode("Node32", [])
+    const node21 = new GraphNode("Node21", [node31])
+    const node22 = new GraphNode("Node22", [node32])
+    const node1 = new GraphNode("Node1", [node21, node22])
+    node41.linkedWith.push(node32)
+    node32.linkedWith.push(node42)
+
+    expect(topologySortBFS(node1).map((current) => current.name)).toEqual(["Node1", "Node21", "Node22", "Node31", "Node41", "Node32", "Node42"])
+  })
+
+  it("Sorts complex graph 3", ()=>{
+    const node42 = new GraphNode("Node42", [])
+    const node41 = new GraphNode("Node41", [])
+    const node31 = new GraphNode("Node31", [node41])
+    const node32 = new GraphNode("Node32", [])
+    const node21 = new GraphNode("Node21", [node31])
+    const node22 = new GraphNode("Node22", [node32])
+    const node1 = new GraphNode("Node1", [node21, node22])
+    node32.linkedWith.push(node42)
+
+    expect(topologySortBFS(node1).map((current) => current.name)).toEqual(["Node1", "Node21", "Node22", "Node31", "Node32", "Node41", "Node42"])
+  })
+
+  it("Sorts simple graph correctly", ()=>{
+    const node4 = new GraphNode("Node4", [])
+    const node2 = new GraphNode("Node2", [])
+    const node3 = new GraphNode("Node3", [node4])
+    const node1 = new GraphNode("Node1", [node2, node3])
+
+    expect(topologySortBFS(node1).map((current) => current.name)).toEqual(["Node1", "Node2", "Node3", "Node4"])
+  })
+
+  it("Sorts graph correctly", ()=>{
+    const node21 = new GraphNode("Node21", [])
+    const node22 = new GraphNode("Node22", [])
+    const node2 = new GraphNode("Node2", [node21, node22])
+    const node31 = new GraphNode("Node31", [])
+    const node3 = new GraphNode("Node3", [node31])
+    const node4 = new GraphNode("Node4", [])
+    const node1 = new GraphNode("Node1", [node2, node3, node4])
+
+    expect(topologySortBFS(node1).map((current) => current.name)).toEqual(["Node1", "Node2", "Node3", "Node4", "Node21", "Node22", "Node31"])
+  })
+
+  it("Sorts graph with multiple parents of the element correctly", () => {
+    const {startingPoint} = getDestinationGraphNodes(false)
+
+    expect(topologySortBFS(startingPoint).map((current) => current.name)).toEqual(["Me", "Bartek", "Cecylia", "Alicja", "Janusz", "Tamara", "Jarek", "Patrycja", "Piotr"])
+  })
+
+  it("Sorts graph with multiple parents of the element in the middle correctly", ()=>{
+    const {startingPoint, node31} = getDestinationGraphNodes(false)
+    const node311 = new GraphNode("Node311")
+    const node312 = new GraphNode("Node312")
+    node31.linkedWith.push(node311, node312)
+
+    expect(topologySortBFS(startingPoint).map((current) => current.name)).toEqual(["Me", "Bartek", "Cecylia", "Alicja", "Janusz", "Tamara", "Jarek", "Patrycja", "Piotr", "Node311", "Node312"])
   })
 })
--- src/graphs/bfs/index.ts
+++ src/graphs/bfs/index.ts
@@ -1,11 +1,13 @@
 import { randomUUID } from "crypto"
 
+// name as ids should be unique (for now)
 export class GraphNode {
   id: string
   name: string
   linkedWith: GraphNode[]
   isDestination: boolean
   depth?: number
+  parent?: GraphNode
 
   constructor(name: string, linkedWith: GraphNode[] = [], isDestination = false){
     this.id = randomUUID()
@@ -43,4 +45,39 @@ export const getPathBFS = (startingPoint: GraphNode, lookingForName?: string) =>
   return {
     exists: false,
   }
+}
+
+// verify that, this function and tests are correct and try to create harder, more nested etc. test cases
+export const topologySortBFS = (startingPoint: GraphNode) => {
+  const nodesToVisit: GraphNode[] = startingPoint.linkedWith.map((current) => {
+    return Object.assign({}, current)
+  })
+  const visitedNodesIds: Set<string> = new Set([startingPoint.id])
+  let sortedNodes: GraphNode[] = [Object.assign({}, startingPoint)]
+
+  while (nodesToVisit.length > 0){
+    const currentElement = nodesToVisit.shift()
+
+    if(visitedNodesIds.has(currentElement.id)){
+      const {exists: cycleExists} = getPathBFS(new GraphNode("Helper", [...currentElement.linkedWith]), currentElement.name)
+
+      if(cycleExists){
+        throw new Error("Graph has cycles")
+      }
+      const newSortedNodes = topologySortBFS(currentElement)
+      const newSortedNodesIds = newSortedNodes.map((current) => current.id)
+      newSortedNodesIds.forEach((id) => visitedNodesIds.delete(id))
+      sortedNodes = sortedNodes.filter((current) => !newSortedNodesIds.includes(current.id))
+    }
+
+    visitedNodesIds.add(currentElement.id)
+    sortedNodes.push(Object.assign({}, currentElement) )
+
+    nodesToVisit.push(...currentElement.linkedWith.map((current) => {
+      return Object.assign({parent: currentElement}, current)
+    }))
+
+  }
+
+  return sortedNodes
 }
